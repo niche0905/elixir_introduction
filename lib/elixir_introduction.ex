095de4868a379120141274ff87aff5d024a2ca22
Niche <73772820+niche0905@users.noreply.github.com>
2025-02-18 12:08:34 +0900

    핀 연산자 ( ^ )
    핀 연산자로 기존의 값과 매칭되도록 강제하는 연산자이다

--- lib/elixir_introduction.ex
+++ lib/elixir_introduction.ex
@@ -15,7 +15,7 @@ defmodule ElixirIntroduction do
   def hello do
     a = 1
     IO.puts(a)  # 1
-    [1, a, 3] = [1, 2, 3]
+    [1, ^a, 3] = [1, 2, 3]
     IO.puts(a)  # 2
     # 기존의 값을 무시하고 새로 매칭하는 경우
   end
