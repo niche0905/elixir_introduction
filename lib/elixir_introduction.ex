7fa46c75c84beed950c7249a507347b7487a47d3
Niche <73772820+niche0905@users.noreply.github.com>
2025-02-18 12:12:41 +0900

    패턴 매칭 연습 문제 2
    패턴 매칭이 되는 경우와 안되는 경우를 알 수 있다

--- lib/elixir_introduction.ex
+++ lib/elixir_introduction.ex
@@ -13,10 +13,8 @@ defmodule ElixirIntroduction do
 
   """
   def hello do
-    a = 1
-    IO.puts(a)  # 1
-    [1, ^a, 3] = [1, 2, 3]
-    IO.puts(a)  # 2
-    # 기존의 값을 무시하고 새로 매칭하는 경우
+    [a, b, a] = [1, 2, 3] # X
+    [c, d, c] = [1, 1, 2] # X
+    [e, f, e] = [1, 2, 1] # O
   end
 end
