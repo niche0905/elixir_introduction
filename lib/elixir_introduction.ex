63706be3b7ed5bee3d474765c2be16cd562e1164
Niche <73772820+niche0905@users.noreply.github.com>
2025-02-18 12:51:32 +0900

    정수형
    엘릭서도 파이썬과 비슷하게 값이 커질수록 알아서 언어에서 처리해 주는것ㅇ을 알 수 있었다

--- lib/elixir_introduction.ex
+++ lib/elixir_introduction.ex
@@ -13,11 +13,10 @@ defmodule ElixirIntroduction do
 
   """
   def hello do
-    name = "elixir"
-    IO.puts(name)     # "elixir"
-    cap_name = String.capitalize(name)
-    IO.puts(cap_name) # "Elixir"
-    # name의 내부 상태를 변경하는 것이 아닌
-    # 잘 조립하여 데이터를 변형하고 새로 할당 (효율적인 메모리로 레퍼런스 등 사용)
+    1234
+    0xcafe
+    0o124
+    0b110011
+    100_000_000_000_000_000_000_000_000_000_000_000
   end
 end
