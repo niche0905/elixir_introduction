5ad567cd732f7122c650173a943a99b45a9d2e97
Niche <73772820+niche0905@users.noreply.github.com>
2025-02-18 11:35:28 +0900

    패턴 매칭 할당문
    엘릭서의 할당문은 여타 다른 언어의 할당문과 다르다

--- lib/elixir_introduction.ex
+++ lib/elixir_introduction.ex
@@ -13,6 +13,10 @@ defmodule ElixirIntroduction do
 
   """
   def hello do
-    :world
+    a = 1
+    1 = a
+    # 2 = a error 매칭
+    # 매칭으로 변수의 변숫값을 변경하려면 변수가 좌변에
+    # 우변의 있는 변수는 상수로 취급
   end
 end
