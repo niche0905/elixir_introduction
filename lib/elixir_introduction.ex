d36f9778917fda97f3d0815205087c7f153ac707
Niche <73772820+niche0905@users.noreply.github.com>
2025-02-18 12:37:34 +0900

    엘릭서의 메모리 관리
    엘릭서의 메모리 관리가 왜 효율적이고 빠른지에 대해 알 수 있는 개념을 주석으로 작성해 두었다

--- lib/elixir_introduction.ex
+++ lib/elixir_introduction.ex
@@ -13,10 +13,9 @@ defmodule ElixirIntroduction do
 
   """
   def hello do
-    list1 = [3, 2, 1]
-    IO.puts(list1) # [3, 2, 1]
-    list2 = [4 | list1]
-    IO.puts(list2) # [4, 3, 2, 1]
-    # 이때 데이터의 복사가 아닌 레퍼런스를 사용
+    # 엘릭서는 여러 프로세스를 사용하는 코드를 작성
+    # 프로세스마다 각자의 힙을 가지는 분산되어 있음
+    # 이때 GC(가비지 컬렉터)가 보다 빠르게 일을 끝마칠 수 있음
+    # 프로세스가 끝날 때 모든 데이터가 사라짐
   end
 end
