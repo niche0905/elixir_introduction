e4918f4d3b3ae77334ceb0b1a55a2b2679596a6d
Niche <73772820+niche0905@users.noreply.github.com>
2025-02-18 12:42:34 +0900

    불변 데이터를 이용한 코딩
    객체를 갱신하는 것이 아닌 복사본을 반환 받는 방식에 대한 개념을 설명하는 코드이다

--- lib/elixir_introduction.ex
+++ lib/elixir_introduction.ex
@@ -13,9 +13,11 @@ defmodule ElixirIntroduction do
 
   """
   def hello do
-    # 엘릭서는 여러 프로세스를 사용하는 코드를 작성
-    # 프로세스마다 각자의 힙을 가지는 분산되어 있음
-    # 이때 GC(가비지 컬렉터)가 보다 빠르게 일을 끝마칠 수 있음
-    # 프로세스가 끝날 때 모든 데이터가 사라짐
+    name = "elixir"
+    IO.puts(name)     # "elixir"
+    cap_name = String.capitalize(name)
+    IO.puts(cap_name) # "Elixir"
+    # name의 내부 상태를 변경하는 것이 아닌
+    # 잘 조립하여 데이터를 변형하고 새로 할당 (효율적인 메모리로 레퍼런스 등 사용)
   end
 end
