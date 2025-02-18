f6d0b3b1ea70c039d2ffbb3bd1f5bd98c147a550
Niche <73772820+niche0905@users.noreply.github.com>
2025-02-18 12:17:28 +0900

    패턴 매칭 연습 문제 3
    패턴 매칭이 되는 경우와 안도는 경우를 ^(핀 연산자)를 통해 알아 볼 수 있다

--- lib/elixir_introduction.ex
+++ lib/elixir_introduction.ex
@@ -13,8 +13,13 @@ defmodule ElixirIntroduction do
 
   """
   def hello do
+    a = 2
+
     [a, b, a] = [1, 2, 3] # X
-    [c, d, c] = [1, 1, 2] # X
-    [e, f, e] = [1, 2, 1] # O
+    [a, b, a] = [1, 1, 2] # X
+    a = 1                 # O
+    ^a = 2                # O
+    ^a = 1                # X
+    ^a = 2 - a            # X
   end
 end
