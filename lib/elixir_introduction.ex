2d278a6aa6c9e8f6aa78f8a1a856e00fdb0ad9ac
Niche <73772820+niche0905@users.noreply.github.com>
2025-02-18 11:50:18 +0900

    패턴 매칭 사례
    패턴 매칭이 성립하는 경우와 그렇지 않은 경우의 패턴 매칭

--- lib/elixir_introduction.ex
+++ lib/elixir_introduction.ex
@@ -13,15 +13,15 @@ defmodule ElixirIntroduction do
 
   """
   def hello do
-    list = [1, 2, [3, 4, 5]]
-    [a, b, c] = list
-    [d, e, f] = c
-    IO.puts(a)
-    IO.puts(b)
-    IO.puts(c)
-    IO.puts(d)
-    IO.puts(e)
-    IO.puts(f)
-    # 더 복잡한 경우 패턴 매칭 양상
+    list = [1, 2, 3]
+    [a, 2, b] = list
+    IO.puts(a)  # 1
+    IO.puts(b)  # 3
+    # 패턴 매칭 사례
+
+    # 아래의 경우 패턴 매칭 실패
+    # list [1, 2, 3]
+    # [a, 1, b] = list error
+    # 좌변과 우변이 매칭되지 않으므로
   end
 end
