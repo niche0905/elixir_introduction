3761b1e86ad188d37dc38f6ab136381a99a715fc
Niche <73772820+niche0905@users.noreply.github.com>
2025-02-19 16:51:21 +0900

    맵 접근
    맵에 접근하여 사용하는 방법이다

--- lib/elixir_introduction.ex
+++ lib/elixir_introduction.ex
@@ -13,17 +13,16 @@ defmodule ElixirIntroduction do
 
   """
   def hello do
-    # 맵
-    %{key => value, key => value}
+    # 맵 접근
     status = %{"AL" => "Alabama", "WI" => "Wisconsin"}
+    IO.puts(status["AL"]) # "Alabama"
+    IO.puts(status["TX"]) # nil (null)
+
     responses = %{{:error, :enoent} => :fatal, {:error, :busy} => :retry}
-    color = %{:red => 0xff0000, :green => 0x00ff00, :blue => 0x0000ff}
-    # == %{blue: 255, green:65280, red: 16711680}
-    numbers = %{"one" => 1, :two => 2, {1, 1, 1} => 3}
-    name = "Jose Valim"
-    names = %{String.downcase(name) => name}
-    # 키워드 리스트와 다른 점은 맵은 하나의 키를 한번만 사용 가능하다
-    # 키워드 리스트의 경우 동일 키를 여러개 가질 수 있다
-    # 크기가 커질수록 맵이 효율적이며 패턴 매칭에 사용할 수 있다
+    IO.puts(responses[{:error, :busy}]) # :retry
+
+    colors = %{:red => 0xff0000, :green => 0x00ff00, :blue => 0x0000ff}
+    IO.puts(colors[:red]) # 16711680
+    IO.puts(colors.green) # 65280 <- 아톰의 경우 .으로 사용할 수도 있다
   end
 end
