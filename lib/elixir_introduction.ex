f4f8e4ffac74116cbf17154667a0e36685804dfe
Niche <73772820+niche0905@users.noreply.github.com>
2025-02-19 17:30:56 +0900

    식별자
    식별자 규칙과 컨벤션등에 대해 알아보았다
    식별자의 경우 문자와 _로 시작하고 뒤에는 문자, 숫자, _가 올 수 있고 ?와 !도 붙일 수 있다
    모듈과, 레코드, 프로토콜 같은 경우 영문 대문자로 시작하고,
    식별자의 경우 소문자로 이루어져있고 _로 구분한다
    _로 시작하는 식별자는 사용되지 않아도 문제가 되지 않는다

--- lib/elixir_introduction.ex
+++ lib/elixir_introduction.ex
@@ -13,16 +13,11 @@ defmodule ElixirIntroduction do
 
   """
   def hello do
-    # 날짜와 시간
-    t1 = Time.new(12, 34, 56)
-    IO.puts(t1) # {:ok, ~T[12:34:56]}
-
-    {:ok, t1} = Time.new(12, 34, 56)
-    t2 = ~T[12:34:56.78]
-
-    t1 == t2 # false
-
-    Time.add(t1, 3600)  # ~T[13:34:56.000000]
-    Time.add(t1, 3600, :millisecond)  # ~T[12:34:59.600000]
+    # 식별자
+    name
+    _age
+    _42
+    wtf?
+    number_of_date
   end
 end
