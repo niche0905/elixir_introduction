cd35c123eb475803dd3313cb20e8cc42f16f117f
Niche <73772820+niche0905@users.noreply.github.com>
2025-02-19 17:24:16 +0900

    날짜와 시간2
    시간을 다루는 방법을 알게 되었고 시길 문법으로 ~T[...], ~N[...], ~U[...] 등을 알게 되었다
    ~N은 NaiveDateTime을 타임존 정보 없이 날짜와 시간 정보만 저장한다
    ~U는 DateTime으로 타임존 관련된 처리를 할 수 있다

--- lib/elixir_introduction.ex
+++ lib/elixir_introduction.ex
@@ -14,15 +14,15 @@ defmodule ElixirIntroduction do
   """
   def hello do
     # 날짜와 시간
-    d1 = Date.new(2018, 12, 25)
-    IO.puts(d1) # {:ok, ~D[2018-12-25]}
+    t1 = Time.new(12, 34, 56)
+    IO.puts(t1) # {:ok, ~T[12:34:56]}
 
-    {:ok, d1} = Date.new(2018, 12, 25)
-    d2 = ~D[2018-12-25]
-    IO.puts(d1 == d2) # true
+    {:ok, t1} = Time.new(12, 34, 56)
+    t2 = ~T[12:34:56.78]
 
-    IO.puts(Date.day_of_week(d1)) # 2
-    IO.puts(Date.add(d1, 7))  # ~D[2019-01-01]
-    inspect(d1, struct: false)
+    t1 == t2 # false
+
+    Time.add(t1, 3600)  # ~T[13:34:56.000000]
+    Time.add(t1, 3600, :millisecond)  # ~T[12:34:59.600000]
   end
 end
