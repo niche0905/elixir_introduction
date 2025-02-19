787b90e0626088e6990b1f7fa3289b5ccd9fce39
Niche <73772820+niche0905@users.noreply.github.com>
2025-02-19 16:46:33 +0900

    맵
    엘릭서에서의 맵의 존재이유와 사용 방법등에 대해 알아보았다
    일반적으로 키-값 자료구조가 필요할 때 맵을 사용한다

--- lib/elixir_introduction.ex
+++ lib/elixir_introduction.ex
@@ -13,10 +13,17 @@ defmodule ElixirIntroduction do
 
   """
   def hello do
-    # 키워드 리스트
-    [name: "Dave", city: "Dallas", likes: "Programming"]
-    # ->
-    [{:name, "Dave"}, {:city, "Dallas"}, {:likes, "Programming"}]
-    # name: "Dave"를 알아서 {:name, "Dave"} 로 바꾸어 준다다
+    # 맵
+    %{key => value, key => value}
+    status = %{"AL" => "Alabama", "WI" => "Wisconsin"}
+    responses = %{{:error, :enoent} => :fatal, {:error, :busy} => :retry}
+    color = %{:red => 0xff0000, :green => 0x00ff00, :blue => 0x0000ff}
+    # == %{blue: 255, green:65280, red: 16711680}
+    numbers = %{"one" => 1, :two => 2, {1, 1, 1} => 3}
+    name = "Jose Valim"
+    names = %{String.downcase(name) => name}
+    # 키워드 리스트와 다른 점은 맵은 하나의 키를 한번만 사용 가능하다
+    # 키워드 리스트의 경우 동일 키를 여러개 가질 수 있다
+    # 크기가 커질수록 맵이 효율적이며 패턴 매칭에 사용할 수 있다
   end
 end
