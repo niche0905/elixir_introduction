ed274e799a05c3deb87805b487046f0e2e5037dd
Niche <73772820+niche0905@users.noreply.github.com>
2025-02-19 16:37:10 +0900

    키워드 리스트
    편하게 사용할 수 있는 키워드 리스트로 {아톰, 값}으로 이루어진 2튜플로 변경하여 준다
    함수의 마지막 인자가 키워드 리스트라면 대괄호를 생략할 수도 있다

--- lib/elixir_introduction.ex
+++ lib/elixir_introduction.ex
@@ -13,13 +13,10 @@ defmodule ElixirIntroduction do
 
   """
   def hello do
-    # 엘릭서의 리스트
-    IO.puts([1, 2, 3] ++ [4, 5, 6])     # [1, 2, 3, 4, 5, 6]
-    IO.puts([1, 2, 3, 4] -- [2, 4])     # [1, 3]
-    IO.puts(1 in [1, 2, 3, 4])          # true
-    IO.puts("wombat" in [1, 2, 3, 4])   # false
-    # 엘릭서에서 리스트는 linked list로 random access에는 아쉬운 성능이지만
-    # 순회에서는 강점을 가지고 가장ㅇ 중요한 재귀적으로 리스트를 정의하는 방식이
-    # 엘릭서 프로그래밍의 핵심이다. (불변의 데이터를 가리키는 것으로 해결하기에)
+    # 키워드 리스트
+    [name: "Dave", city: "Dallas", likes: "Programming"]
+    # ->
+    [{:name, "Dave"}, {:city, "Dallas"}, {:likes, "Programming"}]
+    # name: "Dave"를 알아서 {:name, "Dave"} 로 바꾸어 준다다
   end
 end
