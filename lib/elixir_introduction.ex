97dfb538992575c74e3d4b22cbb67d1fdac6f2b5
niche0905 <yudle537@gmail.com>
2025-03-01 22:54:29 +0900

    함수 연습문제
    함수의 연습문제를 임의로 풀어보았다

--- lib/elixir_introduction.ex
+++ lib/elixir_introduction.ex
@@ -13,8 +13,16 @@ defmodule ElixirIntroduction do
 
   """
   def hello do
-    # 함수에서 패턴 매칭
-    swap = fn {a, b} -> {b, a} end
-    swap.({6, 8})       # {8, 6}
+        # list_concat.([:a, :b], [:c, :d])
+        # => [:a, :b, :c, :d]
+        list_concat = fn (list1, list2) -> [list1[0], list1[1], list2[0], list2[1]] end
+
+        # sum.(1, 2, 3)
+        # => 6
+        sum = fn (a, b, c) -> a + b + c end
+
+        # pair_tuple_to_list.({1234, 5678})
+        # => [1234, 5678]
+        pair_tuple_to_list = fn (tuple1) -> [tuple1[0], tuple1[1]] end
   end
 end
