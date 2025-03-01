ef0460b2f8e74c3a78fcfd1d024787fe3f667b70
niche0905 <yudle537@gmail.com>
2025-03-02 00:08:57 +0900

    함수를 반환하는 함수
    함수의 리턴값을 함수로 하여 함수를 리턴하는 함수를 만들었다

--- lib/elixir_introduction.ex
+++ lib/elixir_introduction.ex
@@ -13,29 +13,8 @@ defmodule ElixirIntroduction do
 
   """
   def hello do
-    # 인자 3개를 받는 함수를 만들어보자
-    # 앞의 인자 2개가 모두 0이면 "FizzBuzz"
-    # 첫 인자만 0이면 "Fizz"
-    # 두 번째 인자만 0이면 "Buzz"
-    # 위 상황에 해당하지 않으면 세 번째 인자를 반환하는 함수
-    fizzbuzz = fn
-        (0, 0, _) -> "FizzBuzz"
-        (0, _, _) -> "Fizz"
-        (_, 0, _) -> "Buzz"
-        (_, _, r) -> r
-        end
-
-    # rem(a, b) 연산자는 a를 b로 나눈 나머지를 반환한다.
-    # n이라는 정수를 받아 위에서 만든 fizzbuzz 함수에 rem(n, 3), rem(n,5), n을 인자로 전달하는 함수를 만들어라
-    func = fn (n) -> fizzbuzz.(rem(n, 3), rem(n, 5), n) end
-
-    # 정답 확인
-    IO.puts(func.(10))   # Buzz
-    IO.puts(func.(11))   # 11
-    IO.puts(func.(12))   # Fizz
-    IO.puts(func.(13))   # 13
-    IO.puts(func.(14))   # 14
-    IO.puts(func.(15))   # FizzBuzz
-    IO.puts(func.(16))   # 16
+    fun1 = fn -> fn -> "Hello" end end
+    fun1.()     # #Function
+    fun1.().()  # "Hello"
   end
 end
