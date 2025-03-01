d41ceeea031351b404eb3773655ab07fdb1ec005
niche0905 <yudle537@gmail.com>
2025-03-01 22:33:49 +0900

    함수
    함수의 예제로 가장 간단한 함수 sum을 만들어보았다

--- lib/elixir_introduction.ex
+++ lib/elixir_introduction.ex
@@ -13,32 +13,8 @@ defmodule ElixirIntroduction do
 
   """
   def hello do
-    # with 사용 시 주의사항
-    mean = with # 이렇게 사용하면 안된다
-            count = Enum.count(values),
-            sum   = Enum.sum(values)
-           do
-            sum / count
-           end
-
-    # 이렇게 사용해야 한다다
-    mean = with count = Enum.count(values), # 첫 번째 파라미터를 with와 같은 줄에
-            sum   = Enum.sum(values)
-           do
-            sum / count
-           end
-
-    # 괄호를 사용해도 좋다
-    mean = with(
-            count = Enum.count(values), # 첫 번째 파라미터를 with와 같은 줄에
-            sum   = Enum.sum(values)
-           ) do
-            sum / count
-           end
-
-    # do를 단축 문법으로 사용하는 방법이다
-    mean = with count = Enum.count(values),
-            sum   = Enum.sum(values)
-           do: sum / count
+    # 함수
+    my_sum = fn (a, b) -> a + b end
+    IO.puts(my_sum.(1, 2))
   end
 end
