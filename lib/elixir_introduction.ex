c14b18ab9d34009a3d92060d68faff4cec04d852
niche0905 <yudle537@gmail.com>
2025-03-01 23:17:58 +0900

    함수 연습문제 해답
    함수 연습문제에서 틀렸던 부분을 수정하였다

--- lib/elixir_introduction.ex
+++ lib/elixir_introduction.ex
@@ -15,7 +15,7 @@ defmodule ElixirIntroduction do
   def hello do
         # list_concat.([:a, :b], [:c, :d])
         # => [:a, :b, :c, :d]
-        list_concat = fn (list1, list2) -> [list1[0], list1[1], list2[0], list2[1]] end
+        list_concat = fn (list1, list2) -> list1 ++ list2 end   # 더 효율적인 방법
 
         # sum.(1, 2, 3)
         # => 6
@@ -23,6 +23,6 @@ defmodule ElixirIntroduction do
 
         # pair_tuple_to_list.({1234, 5678})
         # => [1234, 5678]
-        pair_tuple_to_list = fn (tuple1) -> [tuple1[0], tuple1[1]] end
+        pair_tuple_to_list = fn {a, b} -> [a, b] end    # 더 엘릭서스러운 방식
   end
 end
