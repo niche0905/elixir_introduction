e3f7b583c6e8a27a364af8e2a646fbae359fcb45
niche0905 <yudle537@gmail.com>
2025-03-03 21:57:46 +0900

    중첩 함수 연습문제
    중첩 함수 연습문제에서 내가 생각한 답이지만 틀렸다
    컴파일이 되지 않는다
    문자열을 다루는게 정확하지 않아서 생기는 문제 같다

--- lib/elixir_introduction.ex
+++ lib/elixir_introduction.ex
@@ -13,10 +13,11 @@ defmodule ElixirIntroduction do
 
   """
   def hello do
-    add_n = fn n -> (fn other -> n + other end) end
-    add_two = add_n.(2)
-    add_five = add_n.(5)
-    IO.puts(add_two.(3))
-    IO.puts(add_five.(7))
+    # prefix 함수는 호출 시 새로운 함수를 반환하고,
+    # 이 함수(호출된)는 호출하면 첫 번째 문자열과 두 번째 문자열을 공백으로 이은 문자열을 반환한다.
+    prefix = fn first_string -> fn second_string -> first_string + " " + second_string end end
+    mrs = prefix.("Mrs")
+    IO.puts(mrs.("Smith"))
+    IO.puts(prefix.("Elixir").("Rocks"))
   end
 end
