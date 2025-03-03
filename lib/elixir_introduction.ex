16c4af9f5eac9450f72373c0fa8df1145675ca3a
niche0905 <yudle537@gmail.com>
2025-03-03 22:05:50 +0900

    함수를 인자로 예제
    함수를 인자로 사용하는 함수에 대한 예제를 Enum 모듈의 map이라는 함수를 통해 알아보았다

--- lib/elixir_introduction.ex
+++ lib/elixir_introduction.ex
@@ -13,8 +13,9 @@ defmodule ElixirIntroduction do
 
   """
   def hello do
-    times_2 = fn n -> n * 2 end
-    apply = fn (fun, value) -> fun.(value) end
-    IO.puts(apply.(times_2, 6))
+    list = [1, 3, 5, 7, 9]
+    Enum.map(list, fn elem -> elem * 2 end)     # [2, 6, 10, 14, 18]
+    Enum.map(list, fn elem -> elem * elem end)  # [1, 9, 25, 49, 81]
+    Enum.map(list, fn elem -> elem > 6 end)     # [false, false, false, true, true]
   end
 end
