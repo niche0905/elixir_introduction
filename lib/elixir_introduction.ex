13ba9f287035848411bd96200df104d2a5e02582
niche0905 <yudle537@gmail.com>
2025-03-03 22:50:23 +0900

    & 표기법 응용
    & 표기법을 사용해서 기명에도 마찬가지로 동작 가능하고 (이전 코드)
    다른 함수에 함수를 전달하는 우아한 방법으로 C++의 람다가 연상된다

--- lib/elixir_introduction.ex
+++ lib/elixir_introduction.ex
@@ -13,11 +13,8 @@ defmodule ElixirIntroduction do
 
   """
   def hello do
-    l = &length/1
-    IO.puts(l.([1, 3, 5, 7]))
-    len = &Enum.count/1
-    IO.puts(len.([1, 2, 3, 4]))
-    m = &Kernel.min/2
-    IO.puts(m.(99, 88))
+    Enum.map([1, 2, 3, 4], &(&1 + 1))   # [2, 3, 4, 5]
+    Enum.map([1, 2, 3, 4], &(&1 * &1))  # [1, 4, 9, 16]
+    Enum.map([1, 2, 3, 4], &(&1 < 3))   # [true, true, false, false]
   end
 end
