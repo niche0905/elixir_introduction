fbeeb84932e26f6ce07ef6f8e8eb4a71c483003f
niche0905 <yudle537@gmail.com>
2025-03-03 22:02:55 +0900

    함수를 인자로 전달
    함수를 인자로 전달하여 사용할 수 있게 하는 예제를 apply를 만들며 사용해 보았다

--- lib/elixir_introduction.ex
+++ lib/elixir_introduction.ex
@@ -13,11 +13,8 @@ defmodule ElixirIntroduction do
 
   """
   def hello do
-    # prefix 함수는 호출 시 새로운 함수를 반환하고,
-    # 이 함수(호출된)는 호출하면 첫 번째 문자열과 두 번째 문자열을 공백으로 이은 문자열을 반환한다.
-    prefix = fn first_string -> fn second_string -> "#{first_string} #{second_string}" end end
-    mrs = prefix.("Mrs")
-    IO.puts(mrs.("Smith"))
-    IO.puts(prefix.("Elixir").("Rocks"))
+    times_2 = fn n -> n * 2 end
+    apply = fn (fun, value) -> fun.(value) end
+    IO.puts(apply.(times_2, 6))
   end
 end
