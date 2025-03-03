377a5bbab3d7a86cb52dc2b3dcd8a1f03d07e2d4
niche0905 <yudle537@gmail.com>
2025-03-03 21:53:21 +0900

    함수 중첩 인자를 받는 경우
    함수를 중첩해서 사용할 때 인자를 모두 받는 경우를 알아보았다

--- lib/elixir_introduction.ex
+++ lib/elixir_introduction.ex
@@ -13,8 +13,10 @@ defmodule ElixirIntroduction do
 
   """
   def hello do
-    greeter = fn name -> (fn -> "Hello #{name}" end) end
-    dave_greeter = greeter.("Dave")
-    dave_greeter.() # "Hello Dave"
+    add_n = fn n -> (fn other -> n + other end) end
+    add_two = add_n.(2)
+    add_five = add_n.(5)
+    IO.puts(add_two.(3))
+    IO.puts(add_five.(7))
   end
 end
