7b4d607c97f7b26be8a1a3d7f32acef67184ee72
niche0905 <yudle537@gmail.com>
2025-03-10 22:40:48 +0900

    기명 함수 문법
    기명 함수에서 실사용하는 기본 문법을 알아보았다

--- lib/times.exs
+++ lib/times.exs
@@ -1,11 +1,13 @@
 defmodule Times do
   # 기명 함수
-  # 실제 기본 문법
+  # do...end 문법은 개발자의 편의를 위한 문법이다
+  # 일반적으로 한 줄짜리 코드 블록에는 do: 를
+  # 여러 줄짜리 블록에는 do...end를 사용한다
   def duoble(n), do: n * 2
 
   # 여러 줄의 코드를 묶어 do: 사용
-  def greet(greeting, name), do: (
+  def greet(greeting, name) do
     IO.puts(greeting)
     IO.puts("How're you doing, #{name}")
-  )
+  end
 end
