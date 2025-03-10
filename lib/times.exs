22292788fa276c8880209a5042b158f1c5f046d0
niche0905 <yudle537@gmail.com>
2025-03-10 22:36:54 +0900

    기명 함수 기본 문법
    기명 함수의 실제(원래의) 기본 문법이다

--- lib/times.exs
+++ lib/times.exs
@@ -1,11 +1,11 @@
 defmodule Times do
   # 기명 함수
-  # 코드의 구조를 나누기 위해 모듈로 묶어서 정의
-  # 아래 double 함수의 아리티는 1로 double/1로 쓸 수 있다
-  def double(n) do
-    n * 2
-  end
-  # 기명 함수의 식별은
-  # 함수의 이름과 파라미터의 개수(아리티)로 식별한다
-  # 관련이 없는 두 함수에는 같은 이름을 붙이지 않는 편이 좋다
+  # 실제 기본 문법
+  def duoble(n), do: n * 2
+
+  # 여러 줄의 코드를 묶어 do: 사용
+  def greet(greeting, name), do: (
+    IO.puts(greeting)
+    IO.puts("How're you doing, #{name}")
+  )
 end
