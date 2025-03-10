4081ae7cdf052ef5e0f9b09f43fb96a35fc87fbe
niche0905 <yudle537@gmail.com>
2025-03-10 22:46:02 +0900

    기명 함수 연습문제
    기명 함수의 연습문제를 내 나름대로 풀어보았다

--- lib/times.exs
+++ lib/times.exs
@@ -1,13 +1,13 @@
 defmodule Times do
-  # 기명 함수
-  # do...end 문법은 개발자의 편의를 위한 문법이다
-  # 일반적으로 한 줄짜리 코드 블록에는 do: 를
-  # 여러 줄짜리 블록에는 do...end를 사용한다
-  def duoble(n), do: n * 2
+  def double(n), do: n * 2
 
-  # 여러 줄의 코드를 묶어 do: 사용
-  def greet(greeting, name) do
-    IO.puts(greeting)
-    IO.puts("How're you doing, #{name}")
-  end
+  # 주어진 숫자에 3을 곱해 반환하는 triple 함수를 구현하라
+  def triple(n), do: n * 3
+
+  # 파일을 컴파일 하는 두 가지 방법을 모두 사용해 IEx에서 실행
+  # iex times.exs <- cd 또는 경로까지
+  # iex> c "times.exs" <- 해당 파일이 바로 있는 곳에서 iex 실행 시 가능
+
+  # 주어진 숫자의 4배의 값을 반환하는 quadruple 함수를 구현하라 (double 함수를 이용하여)
+  def quadruple(n), do: double(double(n))
 end
