33b5c6a6fa47084fb56709077510eb98ec3b2ce3
niche0905 <yudle537@gmail.com>
2025-03-13 22:00:27 +0900

    |> 연산자 예제
    |> 연산자를 사용해서 리스트를 만들어 내는 예제이다

--- lib/elixir_introduction.ex
+++ lib/elixir_introduction.ex
@@ -1,13 +1,8 @@
 defmodule ElixirIntroduction do
   def hello do
     # |> 파이프라인 연산자
-    filing = DB.find_customers
-             |> Orders.for_customers
-             |> sales_tax(2018)
-             |> prepare_filing
-    # |> 연산자는 왼쪽 표현식의 결과를 받아서 오른쪽에 있는 함수의 첫 번째 파라미터에 넣는다
-    # 첫 번째 함수 호출이 반환한 고객 리스트가 두 번째 함수인 for_customes의 인자가 된다
-    # 이 함수가 반환하는 주문 리스트는 sales_tax의 첫 번째 인자가 되며, 2018은 두 번째 인자가 된다
-    # 기본적으로 val \> f(a, b)는 f(val, a, b,)와 같다
+    (1..10) |> Enum.map(&(&1*&1)) |> Enum.filter(&(&1 <40))
+    # 함수의 인자를 전달할 때 괄호를 써야한다
+    # 그렇지 않으면 함수를 축약할 때 쓰는 &와 파이프 연산자가 충돌한다
   end
 end
