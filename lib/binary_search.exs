daca3040bf6fb67d44b96d18ed30323d6256ca24
niche0905 <yudle537@gmail.com>
2025-03-13 20:10:32 +0900

    기명 함수 가드 연습문제3
    가장 적합한 코드를 찾았다
    range를 //_로 step까지 받고 와일드카드로 버려 주어야 경고가 안 생기고,
    안쓰는 변수 앞에는 언더바(_)를 붙이는게 원칙이다

--- lib/binary_search.exs
+++ lib/binary_search.exs
@@ -1,20 +1,20 @@
 defmodule Chop do
   def guess(actual, range) when is_integer(actual) and is_struct(range, Range) do
-    start_value..end_value = range
+    start_value..end_value//_ = range
     guess_helper(actual, div(start_value + end_value, 2), start_value, end_value)
   end
 
-  defp guess_helper(actual, mid, low, high) when actual == mid do
+  defp guess_helper(actual, mid, _low, _high) when actual == mid do
     IO.puts("Is It #{mid}")
     mid
   end
 
-  defp guess_helper(actual, mid, low, high) when actual < mid do
+  defp guess_helper(actual, mid, low, _high) when actual < mid do
     IO.puts("Is It #{mid}")
     guess_helper(actual, div(low + mid - 1, 2), low, mid - 1)
   end
 
-  defp guess_helper(actual, mid, low, high) when actual > mid do
+  defp guess_helper(actual, mid, _low, high) when actual > mid do
     IO.puts("Is It #{mid}")
     guess_helper(actual, div(mid + 1 + high, 2), mid + 1, high)
   end
