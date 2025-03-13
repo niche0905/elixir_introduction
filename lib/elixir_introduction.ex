6256892e744e085f9b08095abf3a9096dba5f38f
niche0905 <yudle537@gmail.com>
2025-03-13 22:21:37 +0900

    모듈 중첩은 허상
    모듈 중첩이 허상인 이유에 대해서 예시를 보여준다
    모든 모듈은 최상위에 정의되고 중첩된 모듈을 직접 정의할 수도 있다는 것이 주 이유이다

--- lib/elixir_introduction.ex
+++ lib/elixir_introduction.ex
@@ -32,5 +32,17 @@ defmodule Outer do
   end
 end
 
-Outer.outer_func()
-Outer.Inner.inner_func()
+# 모듈 중첩
+# 엘릭서의 모듈 중첩은 허상이다
+# 모든 모듈은 최상위에 정의된다
+# 코드상으로 모듈을 중첩해서 정의하더라도
+# 엘릭서는 외부 모듈명을 내부 모듈명 앞에 온점(.)으로 연결해 모듈명을 삼는다
+# 즉, 중첩된 모듈을 직접 정의할 수도 있다는 뜻이다
+defmodule Mix.Tasks.Doctest do
+  def run do
+    IO.puts("run call")
+  end
+end
+
+Mix.Tasks.Doctest.run()
+# 이는 모듈 Mix와 Mix.Tasks.Docetst 사이에 특별한 관계가 없음을 의미한다
