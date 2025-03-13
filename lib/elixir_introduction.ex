209f2edcdf1df71388cca7947b7861e33179f756
niche0905 <yudle537@gmail.com>
2025-03-13 21:53:47 +0900

    |> 파이프라인 연산자
    파이프라인 연산자 (|>)의 설명과 사용처를 간단하게 보여주는 예제이다

--- lib/elixir_introduction.ex
+++ lib/elixir_introduction.ex
@@ -1,27 +1,13 @@
 defmodule ElixirIntroduction do
-  @moduledoc """
-  Documentation for `ElixirIntroduction`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ElixirIntroduction.hello()
-      :world
-
-  """
   def hello do
-    # 함수
-    # 데이터를 변경하는 것을 수행하는 함수는 엘릭서의 핵심이 된다
-    # 변수에 바인딩하여 사용하는 익명 함수를 지금까지 다루어보았다
+    # |> 파이프라인 연산자
+    filing = DB.find_customers
+             |> Orders.for_customers
+             |> sales_tax(2018)
+             |> prepare_filing
+    # |> 연산자는 왼쪽 표현식의 결과를 받아서 오른쪽에 있는 함수의 첫 번째 파라미터에 넣는다
+    # 첫 번째 함수 호출이 반환한 고객 리스트가 두 번째 함수인 for_customes의 인자가 된다
+    # 이 함수가 반환하는 주문 리스트는 sales_tax의 첫 번째 인자가 되며, 2018은 두 번째 인자가 된다
+    # 기본적으로 val \> f(a, b)는 f(val, a, b,)와 같다
   end
-
-  # defp 매크로로 프라이빗 함수를 정의할 수 있다
-  # 모듈 내애서만 호출할 수 있다
-  # 하나의 함수를 일부는 public, 일부는 private으로 설정 할 수 없다
-  # 고로 다음은 잘못된 코드이다
-  def fun(a) when is_list(a), do: true
-  defp fun(a), do: false
 end
