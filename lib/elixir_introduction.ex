13f978666e18464bb9019d8bb552518e5530530b
niche0905 <yudle537@gmail.com>
2025-03-13 23:15:57 +0900

    모듈의 이름
    모듈의 이름의 실체를 알게 되었다

--- lib/elixir_introduction.ex
+++ lib/elixir_introduction.ex
@@ -1,8 +1,18 @@
 defmodule ElixirIntroduction do
   def hello do
-    # 모듈 지시자
-    # 엘릭서에서 다른 모듈을 사용할 때 세가지 지시자를 사용한다
-    # 셋 모두 프로그램이 시작하면 함께 실행되며 문법적 스코프를 가진다
-    # 자시자의 효과는 지시자를 만난 곳부터 코드를 감싸는 블록이 끝날 때까지 유효하다
+    # 모듈의 이름
+    # 모듈 이름은 하나의 아톰이다
+    # 대문자로 시작하는 모듈 이름을 하나 쓰면
+    # 엘릭서는 이를 Elixir. 가 앞에 붙은 아톰으로 바꾼다
+    # IO 모듈은 Elixir.IO가 된다
+    is_atom(IO) # true
+    to_string(IO) # "Elixir.IO"
+    :"Elixir.IO" === IO # true
+    IO.puts(123)
+    :"Elixir.IO".puts(123)
+
+    # 다음과 같은 사용도 가능하다
+    my_io = IO
+    my_io.puts(123)
   end
 end
