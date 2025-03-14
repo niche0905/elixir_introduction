b7a83ad84efb05d59dc2e4899e7eb10215bf5773
niche0905 <yudle537@gmail.com>
2025-03-14 15:55:24 +0900

    리스트
    리스트가 재귀적으로 표현되고 이를 예제를 통해서 알아보았다
    리스트는 머리와 꼬리로 구성되어 있다

--- lib/elixir_introduction.ex
+++ lib/elixir_introduction.ex
@@ -1,23 +1,23 @@
 defmodule ElixirIntroduction do
   def hello do
-    # 소수를 소수점 아래 두 자리까지의 문자열로 바꾸는 함수 [얼랭]
-    # https://www.erlang.org/docs/23/man/io_lib#format-2
-    :io_lib.format("~.2f", [5.12]) # <- 문자열 아님 문자 리스트임
-    # 운영체제의 환경 변수를 얻어오는 함수 [엘릭서]
-    # https://hexdocs.pm/elixir/1.12/System.html#get_env/0
-    System.get_env("HOME")
-    # 파일의 확장자를 반환하는 함수 [엘릭서]
-    # https://hexdocs.pm/elixir/1.12/Path.html
-    Path.extname("file.txt")
-    # 현재 작업 디렉터리를 반환하는 함수 [엘릭서]
-    # https://hexdocs.pm/elixir/1.12/File.html#cwd/0
-    File.cwd()
-    # JSON 형식의 문자열을 엘릭서 자료구조로 바꾸는 함수 (검색만)
-    # https://hexdocs.pm/jason/Jason.html#decode/2
-    # {:ok, data} = Jason.decode("{\"key\": \"value\"}") <- 추가 설치를 해야 하는듯??
-    # 운영체제의 셸에서 명령어를 실행하는 함수
-    # https://hexdocs.pm/elixir/1.15.8/System.html#cmd/3
-    # System.cmd("dir", []) <- 윈도우 환경이라 cmd라 써야함
-    # System.cmd("cmd", ["/c", "dir"])
+    # 리스트
+    # 리스트는 머리(head), 꼬리(tail)로 구성되어 있다
+    # 머리는 값을 담고, 꼬리는 머리를 제외한 나머지 값을 담고있는 리스트다
+    # 이는 리스트의 재귀적 정의다
+    # 빈 리스트
+    []
+    # 파이프 기호(|)를 사용해서 리스트의 머리와 꼬리 사이을 구분할 수 있다
+    [3]
+    [3 | []]
+    # 위 둘은 같은 의미이다
+    [2, 3]
+    [2 | [3]]
+    [2 | [3 | []]]
+    # 위 셋은 같은 의미이다
+    [1, 2, 3]
+    [1 | [2, 3]]
+    [1 | [2 | [3]]]
+    [1 | [2 | [3 | []]]]
+    # 위 넷은 같은 의미이다
   end
 end
