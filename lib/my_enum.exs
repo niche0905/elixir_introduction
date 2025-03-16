c1e8bc4984add264f71c72553011afe0a433868f
niche0905 <yudle537@gmail.com>
2025-03-16 23:45:09 +0900

    Enum 연습문제2
    gpt가 개선해준 코드이다
    이처럼 해야 의도한 대로 작동하고 Enum 모듈에 구현되어 있는 것과 최대한 비슷하게 동작할 것이다

--- lib/my_enum.exs
+++ lib/my_enum.exs
@@ -10,9 +10,19 @@ defmodule MyEnum do
   def all?([single], func), do: func.(single)
   def all?([head | tail], func), do: func.(head) and all?(tail, func)
 
+  def gpt_all?([], func), do: true  # 빈 리스트의 경우 일반적으로 true
+  def gpt_all?([single], func), do: func.(single)
+  def gpt_all?([head | tail], func), do: func.(head) && gpt_all?(tail, func) # truthy/falsy 개념이 적용되는 &&가 적합하다
+
   # each
   def each([], func), do: []
   def each([head | tail], func), do: [func.(head) | each(tail, func)]
+
+  def gpt_each([], func), do: :ok # 내가 위에 구현한 것은 Enum.map 이였다
+  def gpt_each([head | tail], func) do
+    func.(head)
+    gpt_each(tail, func)
+  end
 
   # filter
   def filter([], func), do: []
@@ -24,6 +34,15 @@ defmodule MyEnum do
     end
   end
 
+  def gpt_filter([], func), do: []
+  def gpt_filter([head | tail], func) do
+    if func.(head) do
+      [head | gpt_filter(tail, func)] # 오타 있음
+    else
+      gpt_filter(tail, func) # 오타 있음
+    end
+  end
+
   # split 못 품
   def split(list, n), do: split_helper(list, n)
 
@@ -31,10 +50,21 @@ defmodule MyEnum do
   def split_helper([head | tail], 0), do: [head | tail]
   def split_helper(list, n), do: {split_helper(list, n)}
 
+  def gpt_split(list, 0), do: {[], list}
+  def gpt_split([], _n), do: {[], []}
+  def gpt_split([head | tail], n) when n > 0 do # n 음수 입력 방지용(없으면 무한루프)
+    {left, right} = gpt_split(tail, n - 1)
+    {[head | left], right}
+  end
+
   # take
   def take([], n), do: []
   def take(list, 0), do: []
   def take([head | tail], n) when do: [head | take(tail, n - 1)]
+
+  def gpt_take([], n), do: []
+  def gpt_take(_list, 0), do: []  # 사용 안할 변수 와일드 카드
+  def gpt_take([head | tail], n) when n > 0,  do: [head | gpt_take(tail, n - 1)]  # 오타 + 마찬가지로 음수 방지용
 
 end
 
@@ -48,4 +78,8 @@ defmodule MyList do
   def flatten([]), do: []
   def flatten([head | tail]) when is_list(head), do: [flatten(head) | flatten(tail)]
   def flatten([head | tail]), do: [head | flatten(tail)]
+
+  def gpt_flatten([]), do: []
+  def flagpt_flattentten([head | tail]) when is_list(head), do: flatten(head) ++ flatten(tail)  # 중첩 리스트가 해결 되게 ++ 연산자 사용 ... 기존의 코드는 중첩 리스트가 해결되지 않았음
+  def gpt_flatten([head | tail]), do: [head | gpt_flatten(tail)]
 end
