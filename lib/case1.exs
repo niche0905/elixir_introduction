146047a7a76f36c783065bec8c686197ac5b2680
niche0905 <yudle537@gmail.com>
2025-03-18 14:55:13 +0900

    case 중첩된 패턴 매칭
    중첩된 패턴 매칭을 활용하는 예제이다

---
+++ lib/case1.exs
@@ -0,0 +1,12 @@
+
+# 중첩된 패턴 매칭
+
+defmodule Users do
+  dave = %{ name: "Dave", state: "TX", likes: "programming" }
+  case dave do
+    %{state: some_state} = person ->
+      IO.puts("#{person.name} lives in #{some_state}")
+    _ ->
+      IO.puts("No matches")
+  end
+end
