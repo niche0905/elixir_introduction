6e55a1a3af48a45878f7801582d53cc845d2ca73
niche0905 <yudle537@gmail.com>
2025-03-18 14:58:04 +0900

    case 가드 조건절
    case에서 가드 조건절을 활용하는 예제이다

---
+++ lib/case2.exs
@@ -0,0 +1,12 @@
+
+# 가드 조건절
+
+defmodule Users do
+  dave = %{ name: "Dave", age: 27 }
+  case dave do
+    person = %{age: age} when is_number(age) and age >= 21 ->
+      IO.puts("You are cleared to enter the Foo Bar, #{person.name}")
+    _ ->
+      IO.puts("Sorry, no admission")
+  end
+end
