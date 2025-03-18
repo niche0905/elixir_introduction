7aaf32936a1887d070a9e3cf94c6247d4913739f
niche0905 <yudle537@gmail.com>
2025-03-18 14:40:59 +0900

    fizzbuzz2 예제
    Enum.map을 사용하여 숫자를 문자열로 변경하는 예제이다

---
+++ lib/fizzbuzz2.ex
@@ -0,0 +1,22 @@
+
+# Enum.map 사용
+
+defmodule FizzBuzz2 do
+
+  def upto(n) when n > 0 do
+    1..n |> Enum.map(&fizzbuzz/1)
+  end
+
+  defp fizzbuzz(n) do
+    cond do
+      rem(n, 3) == 0 and rem(n, 5) == 0 ->
+        "FizzBuzz"
+      rem(n, 3) == 0 ->
+        "Fizz"
+      rem(n, 5) == 0 ->
+        "Buzz"
+      true ->
+        n
+    end
+  end
+end
