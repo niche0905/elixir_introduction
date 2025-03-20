2095be643b5080eeb925912eda3aae77fb3fb449
niche0905 <yudle537@gmail.com>
2025-03-20 22:45:44 +0900

    제어문 연습문제 개선
    제어문에 대한 연습문제를 gpt와 함께 개선하였다

--- lib/control_problem.exs
+++ lib/control_problem.exs
@@ -4,17 +4,17 @@
 
 defmodule FizzBuzz do
 
-  def upto(n) when n > 0, 1..n |> fizzbuzz/1
+  def upto(n) when n > 0, do: 1..n |> Enum.map(fizzbuzz/1)
 
   defp fizzbuzz(n)
-  case n do
-    rem(n, 3) == 0 and rem(n, 5) == 0 ->
+  case {rem(n, 3), rem(n, 5)} do
+    {0, 0} ->
       "FizzBuzz"
-    rem(n, 3) == 0 ->
+    {0, _} == 0 ->
       "Fizz"
-    rem(n, 5) == 0 ->
+    {_, 0} == 0 ->
       "Buzz"
-    true ->
+    _ ->
       n
   end
 end
@@ -23,6 +23,8 @@ end
 # 어느 방법이 문제를 가장 잘 표현할까 생각해보자
 
 # 잘 모르겠다
+# 익숙한건 cond (C++의 if-else 느낌) 와 case (case 패턴 매칭)이 익숙하지만
+# 함수형 스타일에 적합한 것은 함수 분리(함수 패턴 매칭 방식)이 적합한 것으로 보인다
 
 # 엘릭서에 내장된 함수들은 두 가지 형식을 가지는 경우가 많다
 # xxx 형식 함수는 성공하면 {:ok, data}, 실패하면 {:error, reason}을 반환하고
@@ -34,4 +36,4 @@ end
 # file = ok!(File.open("somefile"))
 
 def ok!({:ok, data}), do: data
-def ok!({:error, reason}), do: raise("Failed #{reason}")
+def ok!({:error, reason}), do: raise(RuntimeError, message: "Failed #{inspect(reason)}")
